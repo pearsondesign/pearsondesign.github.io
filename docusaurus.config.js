d1efd3d4562861f67f370420ba8b45e71d53d443
pearsondesign <130259250+pearsondesign@users.noreply.github.com>
2023-04-08 19:48:37 -0500

    base url update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const projectName = "pearsondesign.github.io";
 const config = {
   title: "Pearson Design",
   tagline: "Dinosaurs are cool",
-  url: `https://${organizationName}.github.io`,
+  url: `http://pearsondesign.co`,
   baseUrl: `/`,
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
@@ -39,15 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
